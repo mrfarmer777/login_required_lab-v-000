da6d1e2245df3c790044abfa82ba7df3451ed8c2
Matt Farmer <mrfarmer777@gmail.com>
2018-07-20 17:29:25 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
 
 
 
-  
+
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,12 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root 'sessions#new'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
   get '/show' => 'secrets#show'
+  post '/'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
