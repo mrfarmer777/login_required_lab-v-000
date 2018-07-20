453eef11fbc4985001a9a38e7b39cdb4ce5cb598
Matt Farmer <mrfarmer777@gmail.com>
2018-07-20 17:35:08 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     if params[:name].present?
       session[:name]=params[:name]
-      redirect_to secret_path
+      redirect_to show_path
     else
       redirect_to sessions_new_path
     end
