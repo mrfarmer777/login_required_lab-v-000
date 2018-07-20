ae5282f0cb2322916d6353a62a36eb018bf80c1b
Matt Farmer <mrfarmer777@gmail.com>
2018-07-20 17:01:53 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,21 @@
 class SessionsController < ApplicationController
 
+  def new
+  end
+
+  def create
+    session[:user_id]=params[:user_id]
+    redirect_to root_path
+  end
+
+
+
+
+
   def current_user
     #get the session user_id if there is one, otherwise, set it equal to an empty array
     session[:user_id] ||= session[:user_id]=[]
   end
+
+  
 end
