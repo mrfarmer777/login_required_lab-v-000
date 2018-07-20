d926852c7949fa923c5aff4b84ba3a43f68deef2
Matt Farmer <mrfarmer777@gmail.com>
2018-07-20 17:09:06 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
 
   def current_user
     #get the session user_id if there is one, otherwise, set it equal to an empty array
-    session[:user_id] ||= session[:user_id]=[]
+    session[:name] ||= session[:name]=[]
   end
 end
